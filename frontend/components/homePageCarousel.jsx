fe9587c4abfb79753f80f97b80ba546330dc2cbb
Matt Allen <Matt.allen1080@gmail.com>
2016-04-03 16:47:33 -0400

    add carousel

--- frontend/components/homePageCarousel.jsx
+++ frontend/components/homePageCarousel.jsx
@@ -1,15 +1,35 @@
 var React = require('react');
 var CarouselMovie = require('./carouselMovie');
 var MovieStore = require('../stores/movieStore');
+var Slider = require('react-slick');
+
 
 var HomePageCarousel = React.createClass({
 
   render: function () {
     var movie = MovieStore.homePageMovies().top_rated_theaters[0];
+    var movie2 = MovieStore.homePageMovies().top_rated_theaters[1];
 
-    return <CarouselMovie movie={movie} />;
+    var settings = {
+      dots: true
+    };
+    return (
+      <div className='carousel-container group'>
+        <Slider {...settings}>
+        <CarouselMovie movie={movie} />
+        <CarouselMovie movie={movie2} />
+        </Slider>
+      </div>
+    );
   }
 
 });
+
+// <Slider {...settings}>
+//
+// <img src='http://placekitten.com/g/400/200' />
+//   <img src='http://placekitten.com/g/400/200' />
+// </Slider>
+
 
 module.exports = HomePageCarousel;
