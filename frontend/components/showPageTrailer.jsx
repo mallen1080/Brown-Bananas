95a9db1965cd7b18d245c55807a345d2efe2816b
Matt Allen <Matt.allen1080@gmail.com>
2016-05-06 11:39:15 -0400

    add current movie check to trailer component

--- frontend/components/showPageTrailer.jsx
+++ frontend/components/showPageTrailer.jsx
@@ -20,7 +20,9 @@ var ShowPageTrailer = React.createClass({
   },
 
   render: function () {
-    var link = this.state.currentMovie.trailer_url + "?rel=0&showinfo=0&autohide=1";
+    var link = this.state.currentMovie.trailer_url;
+    if (link) { link += "?rel=0&showinfo=0&autohide=1"; }
+    
     return (
       <div className="movie-trailer-container">
         <iframe src={link}
