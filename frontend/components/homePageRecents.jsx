1af1d210844d4def705e9b4b799ff3f4b203e2cb
Matt Allen <Matt.allen1080@gmail.com>
2016-05-10 18:13:20 -0400

    add links to recent components

--- frontend/components/homePageRecents.jsx
+++ frontend/components/homePageRecents.jsx
@@ -7,28 +7,33 @@ var HomePageRecents = React.createClass({
 
     var theater = MovieStore.homePageMovies().newest_in_theaters[0];
     var dvd = MovieStore.homePageMovies().newest_on_dvd[0];
+    var link = "#/movies/";
 
     return (
       <div className="home-page-recents group">
 
         <div className="recent-container">
-          <div className="recent-img">
-            <img src={theater.image_url} />
-          </div>
-          <div className="recent-desc">
-            <h3>NEW IN THEATERS</h3>
-            <p>{theater.title}</p>
-          </div>
+          <a href={link + theater.id}>
+            <div className="recent-img">
+              <img src={theater.image_url} />
+            </div>
+            <div className="recent-desc">
+              <h3>NEW IN THEATERS</h3>
+              <p>{theater.title}</p>
+            </div>
+          </a>
         </div>
 
         <div className="recent-container">
-          <div className="recent-img">
-            <img src={dvd.image_url} />
-          </div>
-          <div className="recent-desc">
-            <h3>NEW ON DVD</h3>
-            <p>{dvd.title}</p>
-          </div>
+          <a href={link + dvd.id}>
+            <div className="recent-img">
+              <img src={dvd.image_url} />
+            </div>
+            <div className="recent-desc">
+              <h3>NEW ON DVD</h3>
+              <p>{dvd.title}</p>
+            </div>
+          </a>
         </div>
 
       </div>
